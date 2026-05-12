500addc9c746002e7bb51de64babae8c0a3702bb
Kevin Deng <sxzz@sxzz.moe>
2026-05-12 20:30:13 +0900

    feat!: support rolldown versions >= 1.0.0.rc
    closes #40

--- app/composables/version.ts
+++ app/composables/version.ts
@@ -1,3 +1,5 @@
+import { greaterOrEqual, parse } from 'std-semver'
+
 interface VersionInfo {
   latest: string
   versions: string[]
@@ -7,9 +9,19 @@ export async function getRolldownVersions(): Promise<VersionInfo> {
   const response = await fetch('https://registry.npmjs.org/rolldown').then(
     (r) => r.json(),
   )
+  const versions = Object.keys(response.versions)
+    .toReversed()
+    .filter((v) =>
+      greaterOrEqual(parse(v), {
+        major: 1,
+        minor: 0,
+        patch: 0,
+        prerelease: ['rc'],
+      }),
+    )
   return {
     latest: response['dist-tags'].latest,
-    versions: Object.keys(response.versions).toReversed(),
+    versions,
   }
 }
 
